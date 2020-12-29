b9afd48ff7f7dae9c67e1aecd2755ae87e94c021
Tasha <xtasherx@gmail.com>
2020-12-28 18:51:07 -0600

    why

--- client/src/pages/profile.js
+++ client/src/pages/profile.js
@@ -3,41 +3,14 @@ import React from "react";
 //Bootstrap
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Card from 'react-bootstrap/Card'
-
-//compoenents
-import ReviewCard from '../components/review-card';
 
 
-export default function Profile () {
+export default function Home () {
         return(
-                <Container className="my-5">
-
+        <Container className="my-5">
                 <Row>
-                        <Col md="auto" lg={6}>
-                                <Card style={{ width: '18rem' }}>
-                                        <div className ="circle"></div>
-                                        <Card.Body>
-                                        <Card.Title>Card Title</Card.Title>
-                                        <Card.Text>
-                                        Some quick example text to build on the card title and make up the bulk of
-                                        the card's content.
-                                        </Card.Text>
-                                        </Card.Body>
-                                </Card>
-                        </Col>
-
-                        <Col md="auto" lg={6}>
-
-                        <h5 className="mb-4">Reviews </h5>
-                                <ReviewCard className="mb-2" />
-                                <ReviewCard className="mb-2" />
-                                <ReviewCard className="mb-2" />
-
-                        </Col>
+                        <h1>Hello Profile</h1>
                 </Row>
-
         </Container>
         )
 }
