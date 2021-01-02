52e3410082164edc82e4f26104e608c424f49e47
cascutter <cascutter13@gmail.com>
2021-01-02 11:20:25 -0600

    added pseudocode for pay button logic

--- client/src/components/footer.js
+++ client/src/components/footer.js
@@ -1,11 +1,13 @@
 import React from 'react';
-
 import Row from 'react-bootstrap/Row';
 
 // icons
 import { FaComments, FaDollarSign, FaPenSquare } from "react-icons/fa";
 
 export default function Footer () {
+      
+    //logic for username; api call to database for paypaluser and set to variable
+    //set link as variable href=
     return(
         <footer className="container-fluid text-center main-footer">
             <Row className="d-inline-flex">
@@ -16,7 +18,7 @@ export default function Footer () {
                 <p>message</p>
                 </a>
 
-                <a className="mr-5" href="https://google.com/">
+                <a className="mr-5" href={ "https://paypal.com/paypalme/" } /* add + variable name in {} */>
                 <span> < FaDollarSign size={25} /> </span>
                 <p>pay</p>
                 </a>  
@@ -29,3 +31,4 @@ export default function Footer () {
         </footer>
     )
 };
+
