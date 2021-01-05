aaa3a3a8c6484b7240d4b5e6272dd1df1d8e2071
Tasha <xtasherx@gmail.com>
2021-01-04 20:29:53 -0600

    test

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,8 @@ router.use("/api", apiRoutes);
 // If no API routes are hit, send the React app
 router.use(function(req, res) {
   console.log(__dirname);
-  res.sendFile(path.join(__dirname, "/client/build/index.html"));
+  // res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  res.json({test: "test"});
 });
 
 module.exports = router;
