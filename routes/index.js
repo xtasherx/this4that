bb40a79b3ea3622fb2fccbcbcf245ef10c65ac21
Tasha <xtasherx@gmail.com>
2021-01-04 20:09:52 -0600

    changes to index.js for heroku

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ router.use("/api", apiRoutes);
 // If no API routes are hit, send the React app
 router.use(function(req, res) {
   console.log(__dirname);
-  res.sendFile(path.join(__dirname, "/index.html"));
+  res.sendFile(path.join(__dirname, "/client/build/index.html"));
 });
 
 module.exports = router;
