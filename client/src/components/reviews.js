694824b9dd8ab4d9d7b5d4ed6e7969b7b42eab8e
cascutter <cascutter13@gmail.com>
2021-01-08 19:25:59 -0600

    Removed 'miles' from seedDb; added review seed data to reviews.js

--- client/src/components/reviews.js
+++ client/src/components/reviews.js
@@ -3,26 +3,26 @@
 export default[
   {
     id: 1,
-    review: "Unidentified vessel travelling at sub warp speed, bearing 235.7. Fluctuations in energy readings from it, Captain. All transporters off. A strange set-up, but I'd say the graviton generator is depolarized."
+    review: "I love this community! I’ve always wished we lived in an economy that suited people like me better, people who have more experience and talents to trade than money. This4that allows me access to services and experiences I otherwise might not be able to afford. Not only has it opened up a new world to me, it has allowed me to share my own skills with others who may not have found me otherwise through conventional networks!"
   },
   {
     id: 2,
-    review: "I'm afraid I still don't understand, sir. Now we know what they mean by 'advanced' tactical training. The unexpected is our normal routine. I suggest you drop it, Mr. Data. Damage report!"
+    review: "My favorite thing about this4that is being treated like a person instead of a consumer! This community is all about what you bring to the table and what you can provide for others, instead of being based on what’s in your wallet. Being rich in skill and talent, and being able to share that with others, is much more fulfilling than my normal corporate gig."
   },
   {
     id: 3,
-    review: "Damage report! Wait a minute - you've been declared dead. You can't give orders around here. The unexpected is our normal routine. Yes, absolutely, I do indeed concur, wholeheartedly! What? We're not at all alike!"
+    review: "So excited to have finally found a barter economy and a community based on mutualism outside of our capitalist norm. I love being able to share my gifts and talents with others in return for theirs. this4that is my new favorite place!"
   },
   {
     id: 4,
-    review: "Did you come here for something in particular or just general Riker-bashing? Congratulations - you just destroyed the Enterprise."
+    review: "This past year has been difficult on so many of us, my family included. With a loss in income, it became difficult for me to pay for services that were beyond rent, food, and basic necessities. As a person with specific skills, it has been amazing to be able to trade my craft for additional services my family needs!"
   },
   {
     id: 5,
-    review: "When has justice ever been as simple as a rule book? I guess it's better to be lucky than good. And blowing into maximum warp speed, you appeared for an instant to be in two places at once. Fate. It protects fools, little children, and ships named Enterprise. Fate protects fools, little children and ships named Enterprise."
+    review: "I have had an incredible experience on this4that! Not only have I been able to trade my own time and abilities for others, I love the option to pay for a service as well. It makes it so easy to work out a deal with an individual instead of dealing with a big company. It’s like small business got even smaller and even more personal!"
   },
   {
     id: 6,
-    review: "What's a knock-out like you doing in a computer-generated gin joint like this? Talk about going nowhere fast."
+    review: "This is my new favorite app! I have found so many amazingly talented people which have turned into lasting business and service relationships. I started on this4that offering lawn care services and before I knew it, I had people being referred to me - not just on the app, but outside of it as well! I now have a full blown landscaping side business and it’s all thanks to this4that!"
   }
 ]
